34e4b7b1bd09bb9fc22e56c4fe9aa9f7eaaa97c3
Brett Lykins <brett.lykins@rackspace.com>
2018-10-12 15:14:36 -0500

    Working on SSL enablement

--- gunicorn.py
+++ gunicorn.py
@@ -1,3 +1,5 @@
+from icanhaz.config import CERT_KEY_FILE, CERT_FILE, CERT_BUNDLE_FILE
+
 bind = "0.0.0.0:5000"
 chdir = "/app"
 
@@ -8,9 +10,8 @@ timeout = 300
 accesslog = "-"
 access_log_format = '%(t)s %(p)s %(h)s %({X-Forwarded-For}i)s %(l)s "%(r)s" %(s)s %(b)s "%(f)s"'
 
-# Enable for SSL (Will need to also install SSL packages/extensions into the container if you choose to do this).
-# keyfile = CERT_KEY_FILE
-# certfile = CERT_FILE
-# ca_certs = CERT_BUNDLE_FILE
-# ssl_version = 5
-# ciphers = "HIGH:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
+keyfile = CERT_KEY_FILE
+certfile = CERT_FILE
+ca_certs = CERT_BUNDLE_FILE
+ssl_version = 5
+ciphers = "HIGH:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK"
---
+++ icanhaz/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""
+File for control of variable configuration in the app.
+
+Hostnames, certificates, etc.
+"""
+
+# Cert/Key File Locations
+CERT_KEY_FILE = "/app/key.pem"
+CERT_FILE = "/app/cert.crt"
+CERT_BUNDLE_FILE = "/app/bundle.crt"
+
